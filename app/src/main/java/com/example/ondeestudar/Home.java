37979f8d0ada4c0acf18af8467d10f458b3fffaa
Felipe Linczuk <felipelinczuk@outlook.com>
2022-11-27 18:09:14 -0300

    felipe

--- app/src/main/java/com/example/ondeestudar/Home.java
+++ app/src/main/java/com/example/ondeestudar/Home.java
@@ -2,13 +2,25 @@ package com.example.ondeestudar;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class Home extends AppCompatActivity {
 
+    TextView text_bemVindo;
+    String nome;
+    int id;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        text_bemVindo = findViewById(R.id.text_bemVindo);
+
+        SharedPreferences userlog = getSharedPreferences("userlog", MODE_PRIVATE);
+        nome = userlog.getString("nome","");
+        text_bemVindo.setText("Bem vindo, " + nome);
+
     }
 }
