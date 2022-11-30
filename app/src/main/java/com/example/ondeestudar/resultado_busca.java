d257644ab0c1959281bd3cefdf2df27f3f881009
wanessa <wanessagcamara@outlook.com>
2022-11-30 02:06:12 -0300

    Telas de carregamento, home, pagina do curso e pesquisa.

---
+++ app/src/main/java/com/example/ondeestudar/resultado_busca.java
@@ -0,0 +1,14 @@
+package com.example.ondeestudar;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class resultado_busca extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_resultado_busca);
+    }
+}
